edacab37d0d0856d2e0752f69b9e1531dd449099
SaboorNisha <nisha25804@gmail.com>
2025-01-29 20:59:09 -0800

    Added search feature, like/dislike system, and updated blog templates

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
+#models.py
+
+
 import os
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
@@ -29,10 +32,16 @@ class Post(db.Model):
     blog_post = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', name='fk_user_post'), nullable=True)  # Make nullable initially
+    user = db.relationship('User', backref=db.backref('posts', lazy=True))
+
+    like_count = db.Column(db.Integer, default=0)  # New field
+    dislike_count = db.Column(db.Integer, default=0)  # New field
+
     def __repr__(self):
         return f'<Post {self.Title}>'
         
-class User(UserMixin,db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -41,7 +50,6 @@ class User(UserMixin,db.Model):
     Email = db.Column(db.String(100), nullable=False)
     Password = db.Column(db.String(500), nullable=True)
 
-
     def set_password(self, password):
         self.Password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=10)
 
@@ -49,4 +57,14 @@ class User(UserMixin,db.Model):
         return check_password_hash(self.Password, password)
     
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.Username}>'
+
+class Reaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    reaction_type = db.Column(db.String(50), nullable=False)  # E.g., 'like', 'dislike', 'love'
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    post = db.relationship('Post', backref=db.backref('reactions', lazy=True))
+    user = db.relationship('User', backref=db.backref('reactions', lazy=True))
